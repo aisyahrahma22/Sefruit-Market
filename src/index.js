aa1e43ce59599e3843e49d7d8cf672403ffdb7d8
Aisyah Rahmatul Fajrin <arahmatull@gmail.com>
2024-09-10 19:52:51 +0700

    Update Sefruit FE

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css"; // import css bootstrap module
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { applyMiddleware, createStore } from "redux";
+import { rootReducers } from "./Redux/Reducers";
+import { Provider } from "react-redux";
+import {thunk} from "redux-thunk";
+
+const globalStore = createStore(rootReducers, {}, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={globalStore}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
